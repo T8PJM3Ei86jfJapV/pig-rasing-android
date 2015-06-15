fe8c8a64a63e15a77156768dd06a3d5a3c6437bc
wyp <1140664142@qq.com>
2015-06-16 03:28:51 +0800

    change server return type

--- server/myapp.py
+++ server/myapp.py
@@ -1,13 +1,10 @@
 import MySQLdb
 from flask import Flask, g, request, jsonify
-
 app = Flask(__name__)
 app.debug = True
-
 from sae.const import (MYSQL_HOST, MYSQL_HOST_S,
     MYSQL_PORT, MYSQL_USER, MYSQL_PASS, MYSQL_DB
 )
-
 @app.before_request
 def before_request():
   g.db = MySQLdb.connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS,
